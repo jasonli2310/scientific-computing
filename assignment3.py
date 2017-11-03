302a914bb9f73880aafd449858e7ec5389abbb64
Jason Li <jasonli2310@gmail.com>
2017-11-02 19:00:44 -0500

    arrays for step

--- assignment3.py
+++ assignment3.py
@@ -36,7 +36,7 @@ import math
 
 
 def f(t, y):
-    return t**2
+    return t**4
 
 
 def hermiteCubicSolve(aX, bX, aY, bY, aPrime, bPrime, x):
@@ -49,15 +49,19 @@ def hermiteCubicSolve(aX, bX, aY, bY, aPrime, bPrime, x):
     return (term1 + term2 + term3 + term4)
 
 
+#np.sum([[0, 1], [0, 5]], axis=0)
+
 class ComputeYnew:
     """docstring for step."""
     def __init__(self, t_now, Ynow, dt):
         self.k1 = f(t_now, Ynow)
-        self.k2 = f(t_now + dt/2, Ynow + self.k1 * dt/2)
-        self.k3 = f(t_now + dt/2, Ynow + self.k2 * dt/2)
-        self.k4 = f(t_now + dt, Ynow + self.k3 * dt)
-        self.dY = (self.k1 + 2*self.k2+ 2*self.k2 + self.k4) * dt/2
-        self.Ynew = Ynow + self.dY
+        self.k2 = f(t_now + dt/2, np.sum([Ynow, np.array(self.k1)*dt/2], axis=0))
+
+        self.k3 = f(t_now + dt/2, np.sum([Ynow, np.array(self.k2)*dt/2], axis=0))
+        self.k4 = f(t_now + dt, np.sum([Ynow, np.array(self.k3)*dt], axis=0))
+        self.dY = (np.array(self.k1) + 2*np.array(self.k2)+ 2*np.array(self.k2) + np.array(self.k4)) * dt/2
+        self.Ynew = np.sum([Ynow + self.dY], axis=0)
+
 
 # You should construct an error indicator in the following
 # way.  Once a guess at Ynew exists evaluate f at (t_now+dt,
@@ -91,9 +95,9 @@ class ComputeError:
         self.dY_alt = dt * ( self.w1 * self.Fc1 + self.w2 * self.Fc2 + self.wnew * self.f_new)
 
 
-# a = ComputeYnew(6, 2, 3)
+a = ComputeYnew(6, [2], 3)
 #
-# print(a.Ynew)
+print(a.Ynew)
 #
 # print(a.k1)
 #
@@ -102,50 +106,63 @@ class ComputeError:
 # print(b.testMethod())
 
 
+#
+# timeFrame = [0,2]
+# time = timeFrame[0]
+# endTime = timeFrame[1]
+# Ynow = 1
+# dt = 0.1
+# tolerance = 0.01
+# dtmin = 0.001
+# agrow = 1.25
+# ashrink = 0.8
+#
+# tol = 0.01
+#
+#
+#
+# while time < endTime:
+#     print(time)
+#     tryStep = ComputeYnew(time, Ynow, dt)
+#     errorStep = ComputeError(time, dt, Ynow, tryStep.Ynew, tryStep.k1)
+#     ei = abs(tryStep.dY - errorStep.dY_alt)/ dt
+#
+#
+#     print("ei is ", ei)
+#     print('y new is', tryStep.Ynew)
+#
+#     if (ei < tol or dt == dtmin):
+#         time += dt
+#         Ynow = tryStep.Ynew
+#         print("yup")
+#         print(Ynow)
 
-timeFrame = [0,2]
-time = timeFrame[0]
-endTime = timeFrame[1]
-Ynow = 1
-dt = 0.1
-tolerance = 0.01
-dtmin = 0.05
-agrow = 1.25
-ashrink = 0.8
+        # '''altering dt for better estimations'''
+        # if ei < tol/4:
+        #     dt *= agrow
+        # elif ei > 0.75* tol:
+        #     dt *= ashrink
+        #
+        # '''making sure dt fits at the end of tolerance'''
+        # if time+dt > endTime:
+        #     dt = endTime-time;
+        # elif time + 2*dt > endTime:
+        #     dt = (endTime-time)/2
 
-tol = 0.01
+    # else:
+    #     print("nope")
+    #     if dt/2 < dtmin:
+    #         dt = dtmin
+    #     else:
+    #         dt = dt/2
+    #     print(dt)
 
 
 
-while time < endTime:
-    tryStep = ComputeYnew(time, Ynow, dt)
-    errorStep = ComputeError(time, dt, Ynow, tryStep.Ynew, tryStep.k1)
-    ei = abs(tryStep.dY - errorStep.dY_alt)/ dt
-
-
-    if (ei < tol and dt > dtmin):
-        time += dt
-        Ynow = tryStep.Ynew
-
-        '''altering dt for better estimations'''
-        if ei < tol/4:
-            dt *= agrow
-        elif ei > 0.75* tol:
-            dt *= ashrink
-
-        '''making sure dt fits at the end of tolerance'''
-        if time+dt > endTime:
-            dt = endTime-time;
-        elif time + 2*dt > endTime:
-            dt = (endTime-time)/2
-
-    else:
-        dt /= 2
-
-    print('newStep')
-    print(ei)
-    print(tryStep.Ynew)
-    print(errorStep.dY_alt)
+    # print('newStep')
+    # print(ei)
+    # print(tryStep.Ynew)
+    # print(errorStep.dY_alt)
 
 
 
